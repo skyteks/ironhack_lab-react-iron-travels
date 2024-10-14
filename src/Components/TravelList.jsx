da21a17c0d606f29b65090f2b9c7f9b2d16ffeed
Daniel <Schattenpalkia@googlemail.com>
2024-10-14 20:03:24 +0200

    Add TravelList

--- src/Components/TravelList.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import travelPlansData from "../assets/travel-plans.json";
-
-function TravelList() {
-    const [data, setData] = useState(travelPlansData);
-    
-}
-
-export default TravelList

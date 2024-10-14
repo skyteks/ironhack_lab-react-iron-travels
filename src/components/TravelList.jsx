9a649031cb0075edd62b18ece4db7bfa12946c5d
Daniel <Schattenpalkia@googlemail.com>
2024-10-14 20:15:44 +0200

    Add Remove button

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -3,6 +3,7 @@ import travelPlansData from "../assets/travel-plans.json";
 
 function TravelList() {
     const [data, setData] = useState(travelPlansData);
+    const clickRemove = (removedId) => setData(data.filter((item) => item.id != removedId));
 
     return <div style={{ display: 'flex', flexDirection: 'column', width: "100%", gap: "20px", alignItems: "center" }}>
         {data.map((item) =>
@@ -13,16 +14,16 @@ function TravelList() {
                     <p></p>
                     <p><i>{item.description}</i></p>
                     <p><b>Price: </b>{item.totalCost} €</p>
-                    <div style={{ display: 'flex', flexDirection: 'row', gap: "10px" }}>
-                        {item.allInclusive && <div style={{ color:"white", backgroundColor: "blue", padding: "5px", borderRadius: "5px"}}>All-Inclusive</div>}
-                        {item.totalCost >= 1500 && <div style={{ color:"white", backgroundColor: "blue", padding: "5px", borderRadius: "5px"}}>Premium</div>}
-                        {item.totalCost <= 350 && <div style={{ color:"black", backgroundColor: "darkgreen", padding: "5px", borderRadius: "5px" }}>Great Deal</div>}
+                    <div style={{ display: 'flex', flexDirection: 'row', gap: "10px", marginBottom: "10px" }}>
+                        {item.allInclusive && <div style={{ color: "white", backgroundColor: "blue", padding: "5px", borderRadius: "5px" }}>All-Inclusive</div>}
+                        {item.totalCost >= 1500 && <div style={{ color: "white", backgroundColor: "blue", padding: "5px", borderRadius: "5px" }}>Premium</div>}
+                        {item.totalCost <= 350 && <div style={{ color: "black", backgroundColor: "darkgreen", padding: "5px", borderRadius: "5px" }}>Great Deal</div>}
                     </div>
+                    <button onClick={() => clickRemove(item.id)}>Remove</button>
                 </div>
             </div>
         )}
     </div>
-
 }
 
 export default TravelList;
